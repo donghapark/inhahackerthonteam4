9e307161f3f1d0dac9c647c1f98a28959183f0fe
donghapark <donghaha12@naver.com>
2023-07-08 21:54:56 +0900

    카카오소셜로그인 버튼

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from result import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('allauth.urls')),
-
+    path('', views.home,name='home'),
+    path('login/', include('allauth.urls'),name='login'),
 ]
 
