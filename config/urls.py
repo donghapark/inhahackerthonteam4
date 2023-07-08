8919d9c5f8bd277ec0b3879fa959c89dedb2cb45
donghapark <donghaha12@naver.com>
2023-07-08 21:22:56 +0900

    admin해결

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('login/', include('allauth.urls')),
 
 ]
 
