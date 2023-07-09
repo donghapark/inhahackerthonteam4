e448a4b7a8a11d4138829dd643307c1b9e5766aa
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2023-07-09 17:06:46 +0900

    '정신건강_관련기관_json_파일_추가'

--- result/models.py
+++ result/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+# 성인ADHD, 분노조절장애, 알코올중독, 우울증 
+
+class mental_illness(models.Model):
+    name = models.CharField(max_length=300)
+    count1 = models.FloatField()
+    count2 = models.FloatField()
+    count3 = models.FloatField()
+    count4 = models.FloatField()
+    count5 = models.FloatField()
+    def __str__(self):
+        return self.name
